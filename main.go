c3829df5aac47e3cca28ab967603c3c4b165da1a
Ultimace1314 <assassinoffaith@gmail.com>
2025-03-11 23:27:42 -0400

    setup /app/ and /healthz

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -17,4 +20,10 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
